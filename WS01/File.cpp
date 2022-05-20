c1c1e5802bd8f182edda40e77b930a441221cb5d
Saetbyeol Lim <slim62@myseneca.ca>
2022-05-20 03:37:57 -0400

    WS01 PART1(NOT FINISHED)

---
+++ WS01/File.cpp
@@ -0,0 +1,55 @@
+//**********************************************************************
+//Workshop : #1 (P1)
+//Full Name : Saetbyeol Lim
+//Student ID# : 149814212
+//Email : slim62@myseneca.ca
+//Date : 5/20/2022 
+//Comment :
+//I have done all the coding by myself and only copied the code 
+//that my professor provided to complete my workshops and assignments.
+//**********************************************************************
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#pragma warning(suppress : 4996)
+
+#include <iostream>
+#include "File.h"
+
+using namespace std;
+
+namespace ws01 {
+
+ 
+    FILE* sfptr = nullptr;
+    
+
+    bool openFileForRead() {
+        sfptr = fopen(SHOPPING_DATA_FILE, "r");
+        return sfptr != NULL;
+    }
+
+    bool openFileForOverwrite() {
+        sfptr = fopen(SHOPPING_DATA_FILE, "w");
+        return sfptr != NULL;
+    }
+
+    void closeFile() {
+        if (sfptr) fclose(sfptr);
+    }
+
+    bool freadShoppingRec(ShoppingRec* rec) {
+        int flag = 0;
+        bool success = fscanf(sfptr, "%[^,],%d,%d\n",
+            rec->m_title, &rec->m_quantity, &flag) == 3;
+        rec->m_bought = !!flag;
+        return success;
+    }
+
+    void fwriteShoppintRec(const ShoppingRec* rec) {
+        fprintf(sfptr, "%s,%d,%d\n", rec->m_title, rec->m_quantity, rec->m_bought);
+    }
+
+}
+
+
+
