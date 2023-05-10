b297f336269b861caccb30b4db56240c84ec64e8
Saetbyeol Lim <saetbyeol701@gmail.com>
2023-05-10 00:47:54 -0400

    Seneca Library Application

---
+++ FinalProject/MS5/Utils.h
@@ -0,0 +1,25 @@
+/*************************************************************
+ Final Project Milestone 5
+ Module: Utils
+ File:  utils.h
+ Version: 1.0
+ Date: 2022/8/7
+ Author: Saetbyeol Lim
+ Revision History
+ -------------------------------------------------------------
+ Name            Date            Reason
+ -------------------------------------------------------------
+ -------------------------------------------------------------
+ I have done all the coding by myself and only copied the code
+ that my professor provided to complete my project milestones.
+**************************************************************/
+
+#ifndef SDDS_UTILS_H__
+#define SDDS_UTILS_H__
+#include <iostream>
+namespace sdds {
+
+	int getInt(int min, int max);
+	int getIntForMemberShip(int min, int max);
+}
+#endif // !
