06aabf0f83fd9efe19d45eae849ef685667820b4
Saetbyeol Lim <saetbyeol701@gmail.com>
2023-05-10 00:38:40 -0400

    MS2

---
+++ FinalProject/MS2/Utils.cpp
@@ -0,0 +1,48 @@
+/*************************************************************
+ Final Project Milestone 2
+ Module: Utils
+ File:  utils.cpp
+ Version: 1.0
+ Date: 2022/7/22
+ Author: Saetbyeol Lim
+ Revision History
+ -------------------------------------------------------------
+ Name            Date            Reason
+ -------------------------------------------------------------
+ -------------------------------------------------------------
+ I have done all the coding by myself and only copied the code
+ that my professor provided to complete my project milestones.
+**************************************************************/
+
+#include <iostream>
+#include "Utils.h"
+using namespace std;
+  
+namespace sdds {
+
+	int getInt(int min, int max) {
+		int val;
+		int flag = 0;
+		do {
+			cin >> val;
+			if (cin.fail()) {
+				cout << "Invalid Selection, try again: ";
+				cin.clear();
+				//cin.ignore(1000, '\n');
+				while (cin.get() != '\n');
+			}
+			else if (cin.get() != '\n') {
+				cout << "Invalid Selection, try again: ";
+				while (cin.get() != '\n');
+			}
+			else if (val<min || val>max) {
+				cout << "Invalid Selection, try again: ";
+			}
+			else {
+				flag = 1;
+			}
+
+		} while (flag == 0);
+		return val;
+	}
+}
---
+++ FinalProject/MS2/Utils.h
@@ -0,0 +1,24 @@
+/*************************************************************
+ Final Project Milestone 2
+ Module: Utils
+ File:  utils.h
+ Version: 1.0
+ Date: 2022/7/22
+ Author: Saetbyeol Lim
+ Revision History
+ -------------------------------------------------------------
+ Name            Date            Reason
+ -------------------------------------------------------------
+ -------------------------------------------------------------
+ I have done all the coding by myself and only copied the code
+ that my professor provided to complete my project milestones.
+**************************************************************/
+
+#ifndef SDDS_UTILS_H__
+#define SDDS_UTILS_H__
+#include <iostream>
+namespace sdds {
+	int getInt(int min, int max);
+
+}
+#endif // !
